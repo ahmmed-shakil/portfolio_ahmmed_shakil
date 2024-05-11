0beb4ec561fe8df5ce54ec1233612b921f61b4bf
ahmmed-shakil <shakilahmmed252@gmail.com>
2024-05-11 22:24:16 +0600

    Blg layout fix

--- src/components/AllBlogs/BlogLayout.jsx
+++ src/components/AllBlogs/BlogLayout.jsx
@@ -1,17 +1,28 @@
 import React, { useEffect } from "react";
 import { scrollToTopInsatnt } from "../../utils/scrollToTop";
+import DesktopHeader from "../shared/DesktopHeader";
+import MobileHeader from "../shared/MobileHeader";
+import MobileFooter from "../MobileFooter";
+import DesktopFooter from "../DesktopFooter";
 
 const BlogLayout = ({ children1, children2 }) => {
   useEffect(() => {
     scrollToTopInsatnt();
   }, []);
   return (
-    <div className=" py-10  container px-3 lg:px-0 mx-auto w-full lg:w-10/12">
-      <div className=" grid grid-cols-1 lg:grid-cols-blogPage gap-4">
-        <div>{children1}</div>
-        <div>{children2}</div>
+    <>
+      <DesktopHeader />
+      <MobileHeader />
+      <div className=" py-10  container px-3 lg:px-0 mx-auto w-full lg:w-10/12">
+        <div className=" grid grid-cols-1 lg:grid-cols-blogPage gap-4">
+          <div>{children1}</div>
+          <div>{children2}</div>
+        </div>
       </div>
-    </div>
+
+      <MobileFooter />
+      <DesktopFooter />
+    </>
   );
 };
 
